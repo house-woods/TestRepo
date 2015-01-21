094d3d204bfeb8f834924a62fef761e2ab1990c6
bobo <house_woods@163.com>
2015-01-21 18:12:39 +0800

    replace i with j for git push test

--- arg/main.cpp
+++ arg/main.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(int argc, char* argv[]) { 
    cout << "argc = " << argc << endl; 
-   for(int i = 0; i < argc; i++) 
-      cout << "argv[" << i << "] = " << argv[i] << endl; 
+   for(int j = 0; j < argc; j++) 
+      cout << "argv[" << j << "] = " << argv[j] << endl; 
    return 0; 
 } 
 
